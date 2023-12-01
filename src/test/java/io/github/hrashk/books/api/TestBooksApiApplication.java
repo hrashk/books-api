f690aed00db2f7d8078cc35fc3b1113cc3d95ffe
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-12-01 19:52:48 +0100

    initial commit

---
+++ src/test/java/io/github/hrashk/books/api/TestBooksApiApplication.java
@@ -0,0 +1,30 @@
+package io.github.hrashk.books.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestBooksApiApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:7-alpine")).withExposedPorts(6379);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(BooksApiApplication::main).with(TestBooksApiApplication.class).run(args);
+    }
+
+}
